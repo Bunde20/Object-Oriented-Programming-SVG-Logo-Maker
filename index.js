ee72b68f2680559de43a4229a9660987f24d548b
Steven Bunde <stevenbunde2020@gmail.com>
2023-09-06 11:38:56 -0400

    setup imports for index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
-const filesystem = require('')
+const { writeFile } = require("fs/promises");
+const {Circle, Triangle, Square} = require("./lib/shapes");
+const SVG = require('./lib/svg');
 
 //List of questions to ask the user
 const questions = [
